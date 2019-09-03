4e6b740c5f148d6d20be11bea0b1c6e40c511eda
cannin <cannin@gmail.com>
2019-09-03 09:29:54 -0400

    Added loadSample code

--- app/js/app-cy.js
+++ app/js/app-cy.js
@@ -2,7 +2,15 @@ var jQuery = $ = require('jquery');
 var appUtilities = require('./app-utilities');
 
 module.exports = function() {
+  function loadSample(filename) {
+      return sbgnviz.loadSample(filename, 'app/samples/');
+  }
+
   $(document).ready(function() {
     appUtilities.sbgnNetworkContainer = $('#sbgn-network-container');
+
+    setTimeout(function(){
+        loadSample('neuronal_muscle_signaling.xml');
+    }, 100);
   });
 };
