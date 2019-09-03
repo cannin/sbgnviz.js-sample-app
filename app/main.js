4db5b7968852b81b3c17af1717b3869100179296
cannin <cannin@gmail.com>
2019-09-02 23:46:43 -0400

    Initial modification

--- app/main.js
+++ app/main.js
@@ -1,36 +1,13 @@
 var sbgnviz = require('sbgnviz');
 var filesaverjs = require('filesaverjs');
-window.jQuery = window.$ = require('jquery'); // jQuery should be global because jQuery.qtip extension is not compatible with commonjs
-var cytoscape = require('cytoscape');
 
-require('jquery-expander')($);
-require('bootstrap');
+//window.jQuery = window.$ = require('jquery');
+window.jQuery = window.jquery = window.$ = require('jquery'); 
+
+var cytoscape = require('cytoscape');
 
 var appUtilities = require('./js/app-utilities');
 var appCy = require('./js/app-cy');
-var appMenu = require('./js/app-menu');
-
-// Get cy extension instances
-var cyPanzoom = require('cytoscape-panzoom');
-var cyQtip = require('cytoscape-qtip');
-var cyCoseBilkent = require('cytoscape-cose-bilkent');
-var cyUndoRedo = require('cytoscape-undo-redo');
-var cyClipboard = require('cytoscape-clipboard');
-var cyContextMenus = require('cytoscape-context-menus');
-var cyExpandCollapse = require('cytoscape-expand-collapse');
-var cyEdgeBendEditing = require('cytoscape-edge-bend-editing');
-var cyViewUtilities = require('cytoscape-view-utilities');
-
-// Register cy extensions
-cyPanzoom( cytoscape, $ );
-cyQtip( cytoscape, $ );
-cyCoseBilkent( cytoscape );
-cyUndoRedo( cytoscape );
-cyClipboard( cytoscape, $ );
-cyContextMenus( cytoscape, $ );
-cyExpandCollapse( cytoscape, $ );
-cyEdgeBendEditing( cytoscape, $ );
-cyViewUtilities( cytoscape, $ );
 
 // Libraries to pass sbgnviz
 var libs = {};
@@ -42,6 +19,7 @@ libs.cytoscape = cytoscape;
 sbgnviz({
   networkContainerSelector: '#sbgn-network-container',
   imgPath: 'node_modules/sbgnviz/src/img',
+  
   // whether to fit label to nodes
   fitLabelsToNodes: function () {
     return appUtilities.currentGeneralProperties.fitLabelsToNodes;
@@ -53,9 +31,7 @@ sbgnviz({
   // percentage used to calculate compound paddings
   compoundPadding: function () {
     return appUtilities.currentGeneralProperties.compoundPadding;
-  },
-  undoable: true
+  }
 }, libs);
 
 appCy();
-appMenu();
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app);
 var port = process.env.port || 3000;
 
 server.listen(port, function(){
-  console.log('server listening on port%d', port);
+  console.log('server listening on port %d', port);
 });
 
 app.use(express.static(__dirname));
