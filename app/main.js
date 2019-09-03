92e37366aeaa05e030fae0c8e4d8686682f05544
cannin <cannin@gmail.com>
2019-09-03 11:57:48 -0400

    Removed other JS files Added sbgnviz.register for libs Attempt to load by loadSBGNMLFile

--- app/main.js
+++ app/main.js
@@ -1,12 +1,10 @@
 var sbgnviz = require('sbgnviz');
 var filesaverjs = require('filesaverjs');
 var cytoscape = require('cytoscape');
-//jQuery = jquery = $ = require('jquery');
-//var jQuery = require('jQuery');
-var jQuery = require('jquery');
+var jQuery = $ = require('jquery');
 
-var appUtilities = require('./js/app-utilities');
-var appCy = require('./js/app-cy');
+// Get cy extension instances
+var cyPanzoom = require('cytoscape-panzoom');
 
 // Options
 var options = {
@@ -15,16 +13,25 @@ var options = {
 
     // whether to fit label to nodes
     fitLabelsToNodes: function () {
-        return appUtilities.currentGeneralProperties.fitLabelsToNodes;
+        return false;
     },
     // dynamic label size it may be 'small', 'regular', 'large'
     dynamicLabelSize: function () {
-        return appUtilities.currentGeneralProperties.dynamicLabelSize;
+        return 'regular';
     },
     // percentage used to calculate compound paddings
     compoundPadding: function () {
-        return appUtilities.currentGeneralProperties.compoundPadding;
-    }
+        return 10;
+    },
+    // ???
+    rearrangeAfterExpandCollapse: function () {
+        return false;
+    },
+    // ???
+    animateOnDrawingChanges: function () {
+        return false;
+    },
+    undoable: false
 };
 
 // Libraries to pass sbgnviz
@@ -34,8 +41,73 @@ var libs = {
     filesaverjs: filesaverjs
 };
 
-alert('hello');
+//var filename = 'neuronal_muscle_signaling.xml';
+var filename = 'neuronal_muscle_signaling_color.xml';
+var folder = 'app/samples/';
 
-sbgnviz(options, libs);
+console.log('START');
 
-appCy();
+// Register cy extensions
+cyPanzoom(cytoscape, $);
+
+function loadXMLDoc(path) {
+    var xhttp;
+
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    }
+    else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.open("GET", path, false);
+    xhttp.send();
+
+    console.log(xhttp.responseText);
+
+    return xhttp.responseText;
+}
+
+$(document).ready(function () {
+    function cytoscapeExtensions() {
+        console.log("Cytoscape Extensions");
+
+        var panProps = {
+            fitPadding: 10,
+            fitSelector: ':visible',
+            animateOnFit: true,
+            animateOnZoom: true
+        };
+
+        cy.panzoom(panProps);
+    }
+
+    // Register libs
+    sbgnviz.register(libs);
+
+    var s = sbgnviz(options);
+
+    var cy = s.getCy();
+
+    // register extensions when cy is ready
+    cy.ready(function () {
+        cytoscapeExtensions();
+    });
+
+    setTimeout(function() {
+        //s.loadSample(filename, folder);
+
+        // load xml document
+        var xmlObject = loadXMLDoc(folder + filename);
+        console.log("S: " + xmlObject.toString());
+
+        s.loadSBGNMLFile(new Blob([xmlObject], {
+            type: "text/xml"
+        }));
+
+        //var svgContent = cy.svg({scale: 1, full: true});
+        //console.log("svgContent: " + svgContent);
+        //filesaverjs.saveAs(new Blob([svgContent], {type:"image/svg+xml;charset=utf-8"}), "network.svg");
+    }, 100);
+
+    console.log('Loaded filename: ' + filename);
+});
