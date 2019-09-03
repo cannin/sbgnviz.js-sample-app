abe3404e0be63e149f8de0b3e044446a2ed90a7e
cannin <cannin@gmail.com>
2019-09-03 00:55:11 -0400

    Updated jquery code

--- app/main.js
+++ app/main.js
@@ -1,37 +1,41 @@
 var sbgnviz = require('sbgnviz');
 var filesaverjs = require('filesaverjs');
-
-//window.jQuery = window.$ = require('jquery');
-window.jQuery = window.jquery = window.$ = require('jquery'); 
-
 var cytoscape = require('cytoscape');
+//jQuery = jquery = $ = require('jquery');
+//var jQuery = require('jQuery');
+var jQuery = require('jquery');
 
 var appUtilities = require('./js/app-utilities');
 var appCy = require('./js/app-cy');
 
+// Options
+var options = {
+    networkContainerSelector: '#sbgn-network-container',
+    imgPath: 'node_modules/sbgnviz/src/img',
+
+    // whether to fit label to nodes
+    fitLabelsToNodes: function () {
+        return appUtilities.currentGeneralProperties.fitLabelsToNodes;
+    },
+    // dynamic label size it may be 'small', 'regular', 'large'
+    dynamicLabelSize: function () {
+        return appUtilities.currentGeneralProperties.dynamicLabelSize;
+    },
+    // percentage used to calculate compound paddings
+    compoundPadding: function () {
+        return appUtilities.currentGeneralProperties.compoundPadding;
+    }
+};
+
 // Libraries to pass sbgnviz
-var libs = {};
+var libs = {
+    cytoscape: cytoscape,
+    jQuery: jQuery,
+    filesaverjs: filesaverjs
+};
 
-libs.filesaverjs = filesaverjs;
-libs.jQuery = jQuery;
-libs.cytoscape = cytoscape;
+alert('hello');
 
-sbgnviz({
-  networkContainerSelector: '#sbgn-network-container',
-  imgPath: 'node_modules/sbgnviz/src/img',
-  
-  // whether to fit label to nodes
-  fitLabelsToNodes: function () {
-    return appUtilities.currentGeneralProperties.fitLabelsToNodes;
-  },
-  // dynamic label size it may be 'small', 'regular', 'large'
-  dynamicLabelSize: function () {
-    return appUtilities.currentGeneralProperties.dynamicLabelSize;
-  },
-  // percentage used to calculate compound paddings
-  compoundPadding: function () {
-    return appUtilities.currentGeneralProperties.compoundPadding;
-  }
-}, libs);
+sbgnviz(options, libs);
 
 appCy();
